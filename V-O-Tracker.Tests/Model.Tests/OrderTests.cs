b5d2b4ac6c1b79474ebd7d38cb9bfb75b72a02cf
Min Chang <minchangmhc@gmail.com>
2021-05-14 12:05:25 -0700

    Add passing test for description of order.

--- V-O-Tracker.Tests/Model.Tests/OrderTests.cs
+++ V-O-Tracker.Tests/Model.Tests/OrderTests.cs
@@ -18,5 +18,13 @@ namespace VendorOrderTracker.Tests
       Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Pastry";
+      Order newOrder = new Order(description);
+      string result = newOrder.Description;
+      Assert.AreEqual(description, result);
+    }
   }
 }
